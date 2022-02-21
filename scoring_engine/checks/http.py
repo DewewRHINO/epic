d9429ee74fbfa816b9faa63b6a46d4e51a94e5c6
DewewRHINO <jrjayme@cpp.edu>
2022-02-21 09:29:25 -0800

    Initial

---
+++ scoring_engine/checks/http.py
@@ -0,0 +1,11 @@
+from scoring_engine.engine.basic_check import BasicCheck
+
+
+class HTTPCheck(BasicCheck):
+    required_properties = ['useragent', 'vhost', 'uri']
+    CMD = 'curl -L {2} -A {1}'
+
+    def command_format(self, properties):
+        host_header = 'Host: ' + properties['vhost']
+        host_uri = self.host + ':' + str(self.port) + properties['uri']
+        return (host_header, properties['useragent'], host_uri)
